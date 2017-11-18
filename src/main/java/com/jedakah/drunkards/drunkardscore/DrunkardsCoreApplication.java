84a888b35da593d0720982ea17b73ab47c0d6d3c
Evgenii Shchelovanov <evgenii.shchelovanov@netcracker.com>
2017-11-18 14:19:50 +0300

    Commit initial spring boot project

---
+++ src/main/java/com/jedakah/drunkards/drunkardscore/DrunkardsCoreApplication.java
@@ -0,0 +1,12 @@
+package com.jedakah.drunkards.drunkardscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrunkardsCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrunkardsCoreApplication.class, args);
+	}
+}
