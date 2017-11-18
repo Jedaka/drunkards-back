5fd40cc05d50afd350c4633a86c8dd8e5f2707f0
alsa0116 <aleksei.savin@NetCracker.com>
2017-11-19 00:13:49 +0300

    initial markers

--- front-src/index.js
+++ front-src/index.js
@@ -26,6 +26,32 @@ $(document).ready(() => {
 
     actions.addAction({
         className: "wrap__action-buttons-btn wrap__action-buttons-btn--full",
-        text: "Найти Событие"
+        text: "Принять Участие"
     })
 });
+
+function getMarkers() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/api/events', false);
+  xhr.send();
+  if (xhr.status != 200) {
+    alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+  } else {
+    alert( xhr.responseText ); // responseText -- текст ответа.
+  }
+}
+
+
+function getMarkers() {
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: myLatLng
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
