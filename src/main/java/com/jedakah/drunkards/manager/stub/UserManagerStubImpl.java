b5b3b12a1d47387d3659119f631d9d44f798bf69
georgy <slesarev.georgy@gmail.com>
2017-11-18 17:02:17 +0300

    Implement stub managers

--- src/main/java/com/jedakah/drunkards/manager/stub/UserManagerStubImpl.java
+++ src/main/java/com/jedakah/drunkards/manager/stub/UserManagerStubImpl.java
@@ -5,6 +5,7 @@ import com.jedakah.drunkards.manager.UserManager;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 
 @Profile(value = "stub")
@@ -13,21 +14,31 @@ public class UserManagerStubImpl implements UserManager{
 
     @Override
     public User getUser(String userId) {
-        return null;
+
+        User user = new User();
+        user.setTelephoneNumber("8-800-555-35-35");
+        user.setAge(40);
+
+        return user;
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        User user = new User();
+        user.setTelephoneNumber("8-800-555-35-35");
+        user.setAge(40);
+
+        return Collections.singletonList(user);
     }
 
     @Override
     public User createUser(User user) {
-        return null;
+        return user;
     }
 
     @Override
     public User updateUser(User user) {
-        return null;
+        return user;
     }
 }
