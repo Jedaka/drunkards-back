28e9364132ff268e6899808b2a49da9ae755f581
Evgenii Shchelovanov <evgenii.shchelovanov@netcracker.com>
2017-11-18 16:35:33 +0300

    Implement jpa model

--- src/main/java/com/jedakah/drunkards/entity/AbstractEntity.java
+++ src/main/java/com/jedakah/drunkards/entity/AbstractEntity.java
@@ -1,10 +1,14 @@
 package com.jedakah.drunkards.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class AbstractEntity {
 
+  @Id
+  @GeneratedValue
   private Long id;
   private String description;
 
--- src/main/java/com/jedakah/drunkards/entity/Event.java
+++ src/main/java/com/jedakah/drunkards/entity/Event.java
@@ -1,7 +1,15 @@
 package com.jedakah.drunkards.entity;
 
 import java.util.Date;
+import java.util.List;
+import javax.persistence.Embeddable;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
@@ -12,12 +20,23 @@ import lombok.ToString;
 @EqualsAndHashCode(callSuper = true)
 public class Event extends AbstractEntity {
 
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "host_id")
   private User host;
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(
+      name = "event_user",
+      joinColumns = @JoinColumn(name = "event_id"),
+      inverseJoinColumns = @JoinColumn(name = "drunkard_id")
+  )
+  private List<User> guests;
+  @Embedded
   private Location location;
   private Date createdAt;
   private EventStatus eventStatus;
 
   @Data
+  @Embeddable
   public static class Location {
     private String latitude;
     private String longitude;
