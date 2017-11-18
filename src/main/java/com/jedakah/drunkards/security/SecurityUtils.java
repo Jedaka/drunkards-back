2685d2f35b12b6e30d6692554c78cd92d57cbfbe
alsa0116 <aleksei.savin@NetCracker.com>
2017-11-18 20:39:35 +0300

    added logging to spring security

--- src/main/java/com/jedakah/drunkards/security/SecurityUtils.java
+++ src/main/java/com/jedakah/drunkards/security/SecurityUtils.java
@@ -1,15 +1,14 @@
 package com.jedakah.drunkards.security;
 
-import com.jedakah.drunkards.entity.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtils {
 
-  public User getCurrentUserFromSession() {
+  public MyUserPrincipal getCurrentUserFromSession() {
 
     Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    return (User) authentication.getPrincipal();
+    return (MyUserPrincipal) authentication.getPrincipal();
   }
 
   public String getUserNameFromSession() {
